125f72b234a82983615168eb34b9e809c1d41be0
Johanna <jst@easy-m.de>
2024-02-10 22:44:21 +0100

    feat: :sparkles: add custom theme

--- web/packages/noxite-app/src/main.ts
+++ web/packages/noxite-app/src/main.ts
@@ -1,4 +1,5 @@
-import { VueComponents, WebComponents } from '@easy/ui' // currently throws an ts error, has to be fixed in the future
+// @ts-expect-error easy.ui currently throws a ts error, has to be fixed in the future
+import { VueComponents, WebComponents } from '@easy/ui'
 import messages from '@intlify/unplugin-vue-i18n/messages'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
@@ -7,6 +8,7 @@ import router from './router'
 import App from './App.vue'
 import '@easy/ui/style.css'
 import './style.css'
+import '@/assets/tokens.css'
 
 WebComponents.EasyUI.install()
 
